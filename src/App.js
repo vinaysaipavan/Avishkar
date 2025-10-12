74e132bc1a0cf82fd1852554d02bd52686cebf4c
vinay sai pavan <banduchodevinaysaipavan@gmail.com>
2025-10-12 10:43:46 +0530

    Vinay first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './LetsTalk';
+import './Faq';
+import { Letstalk } from './LetsTalk';
+import { FAQ } from './Faq';
+
+function App() {
+
+  return (
+    <>
+    <Letstalk/>
+    <FAQ />
+    </>
+  );
+}
+
+export default App;
